102dac6d2c78582ab275e764a136039301cf4fed
sanchis <sanchis1992@gmail.com>
2021-02-28 16:31:22 +0100

    create basic layout app

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
+import { useState } from 'react'
+import Head from 'next/head'
+import styles from 'styles/index.module.css'
 
 export default function Home () {
+  const [keyword, setKeyword] = useState('')
+
   return (
-    <p>Home</p>
+    <>
+      <Head>
+        <title>Home - MadHardware</title>
+      </Head>
+      <input type='text' className={`margin-auto ${styles.search}`} name='keyword' placeholder='Introduce tu producto' required onChange={(event) => setKeyword(event.target.value)} value={keyword} />
+    </>
   )
 }
