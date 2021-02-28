8acbe8fa244f5601b9b9bf298c92b5e7632510fa
sanchis <sanchis1992@gmail.com>
2021-02-28 18:26:40 +0100

    split global css in small files

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import { AppLayaout } from 'components/AppLayout'
+import '../styles/variables.css'
 import '../styles/globals.css'
+import '../styles/inputs.css'
+import '../styles/typography.css'
 
-function MyApp ({ Component, pageProps }) {
+export default function MyApp ({ Component, pageProps }) {
   return (
     <AppLayaout>
       <Component {...pageProps} />
     </AppLayaout>
   )
 }
-
-export default MyApp
