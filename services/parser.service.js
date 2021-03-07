ab4443a503d659791033a2e933cd73f3470aff8a
sanchis <sanchis1992@gmail.com>
2021-03-07 23:26:59 +0100

    set log

--- services/parser.service.js
+++ services/parser.service.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 export function searchPcc (text) {
   const url = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
-
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
+  console.log(url)
   return axios.get(`${url}/api/parser/pcc/${text}`)
     .then(res =>
       res.data || null // In case statusCode !== 200 data is undefined
