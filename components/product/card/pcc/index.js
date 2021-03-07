eae1774cc4a216d069d665a2379fea6d79b044c7
sanchis <sanchis1992@gmail.com>
2021-03-07 19:13:16 +0100

    add link product

--- components/product/card/pcc/index.js
+++ components/product/card/pcc/index.js
@@ -1,12 +1,15 @@
 
 import React from 'react'
 import styles from './pcc-card.module.css'
+import Image from 'next/image'
 
 export function PccomponentesCard ({ product }) {
   return (
     <>
       <div className={styles.productCard}>
-        <img src={product.image} />
+        <div className={styles.img}>
+          <Image width='300' height='300' src={product.image} />
+        </div>
         <div className={styles.content}>
           <h3 className={styles.title}>{product.name}</h3>
           <p className={styles.body}>
@@ -16,7 +19,7 @@ export function PccomponentesCard ({ product }) {
         </div>
         <div className={styles.price}>
           <h1>{product.price} €</h1>
-          {/* TODO generate link to shop */}
+          <a className={styles.url} href={product.url} target='_blank' rel='noopener noreferrer'>Ver producto</a>
         </div>
       </div>
     </>
