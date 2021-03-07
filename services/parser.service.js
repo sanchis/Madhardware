d439bc4ec0f4cd22048bffd1fddb50a7b9e8816d
sanchis <sanchis1992@gmail.com>
2021-03-07 22:26:00 +0100

    test base url

--- services/parser.service.js
+++ services/parser.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export function searchPcc (text) {
-  return axios.get(`http://localhost:3000/api/parser/pcc/${text}`)
+  return axios.get(`/api/parser/pcc/${text}`)
     .then(res =>
       res.data || null // In case statusCode !== 200 data is undefined
     )
