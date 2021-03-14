ce2a717ed1f32b21611e0b2d0ed38fbe11c0f537
sanchis <sanchis1992@gmail.com>
2021-03-14 14:18:44 +0100

    fix: product not found

--- pages/search/[text].js
+++ pages/search/[text].js
@@ -3,11 +3,13 @@ import { PccomponentesCard } from 'components/product/card/pcc'
 import { searchPcc } from 'services/parser.service'
 
 export default function SearchResult ({ pcc }) {
+  const notFound = () => <Heading textAlign='center' fontWeight='thin' fontSize='1xl'>Producto no encontrado 😔</Heading>
+
   return (
     <SimpleGrid columns={[1, 2, 3]} gap='10'>
       <div>
         <Heading textAlign='center' mb='5'>Pccomponentes</Heading>
-        {pcc ? <PccomponentesCard product={pcc} /> : <p>Producto no encontrado</p>}
+        {pcc ? <PccomponentesCard product={pcc} /> : notFound()}
       </div>
     </SimpleGrid>
   )
