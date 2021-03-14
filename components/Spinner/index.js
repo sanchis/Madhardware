25b4bc2370ab36fa46eb6e5442caa7b81ba5ae5a
sanchis <sanchis1992@gmail.com>
2021-03-14 12:03:35 +0100

    change colors spinner module dynamically

--- components/Spinner/index.js
+++ components/Spinner/index.js
@@ -1,13 +1,19 @@
 import styles from './spinner.module.css'
 import useLoading from 'hooks/useLoading'
+import { useColorModeValue } from '@chakra-ui/react'
 
 export default function Spinner () {
   const [loading] = useLoading()
+  const bgColor = useColorModeValue('blackAlpha-600', 'whiteAlpha-400') // dark //white
 
   return (loading > 0
     ? (
       <>
-        <div className={styles.overlay} />
+        <div
+          className={styles.overlay} style={{
+            backgroundColor: `var(--chakra-colors-${bgColor})`
+          }}
+        />
         <div className={styles.SkfoldingCube}>
           <div className={`${styles.Skcube1} ${styles.Skcube}`} />
           <div className={`${styles.Skcube2} ${styles.Skcube}`} />
