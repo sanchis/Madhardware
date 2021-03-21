ed7b0a7013f391e8dcf0c0f73e34a877da603d64
sanchis <sanchis1992@gmail.com>
2021-03-21 23:03:26 +0100

    add progress bar

--- components/product/card/index.js
+++ components/product/card/index.js
@@ -1,10 +1,11 @@
 
-import { Box, Center, Heading, Image, SkeletonCircle, SkeletonText, Text, Tooltip } from '@chakra-ui/react'
+import { Box, Center, Heading, Image, Progress, SkeletonCircle, SkeletonText, Text, Tooltip } from '@chakra-ui/react'
 
 export default function ProductCard ({ product, shopColor, loading = false }) {
   const isLoading = () => !product || loading
   return (
     <Box borderWidth='1px' borderColor={shopColor} borderRadius='lg' overflow='hidden'>
+      {isLoading() ? <Progress size='xs' isIndeterminate colorScheme='orange' /> : null}
       <Center>
         <SkeletonCircle width='300px' height='300px' colorScheme='blue' isLoaded={!isLoading()}>
           <Image maxW='300' mt='3' borderRadius='lg' src={product?.image} alt={product?.name} />
