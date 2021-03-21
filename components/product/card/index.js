20827a8812a7030fa339db9cef3daf0d1ea05715
sanchis <sanchis1992@gmail.com>
2021-03-21 22:41:19 +0100

    feat: create generic component for searcher

--- components/product/card/index.js
+++ components/product/card/index.js
@@ -1,24 +1,31 @@
 
-import { Box, Center, Heading, Image, Text, Tooltip } from '@chakra-ui/react'
+import { Box, Center, Heading, Image, SkeletonCircle, SkeletonText, Text, Tooltip } from '@chakra-ui/react'
 
-export default function ProductCard ({ product, shopColor }) {
+export default function ProductCard ({ product, shopColor, loading = false }) {
+  const isLoading = () => !product || loading
   return (
     <Box borderWidth='1px' borderColor={shopColor} borderRadius='lg' overflow='hidden'>
       <Center>
-        <Image maxW='300' src={product.image} alt={product.name} />
+        <SkeletonCircle width='300px' height='300px' colorScheme='blue' isLoaded={!isLoading()}>
+          <Image maxW='300' mt='3' borderRadius='lg' src={product?.image} alt={product?.name} />
+        </SkeletonCircle>
       </Center>
 
       <Box p='6'>
-        <Tooltip label={product.name}>
-          <Heading align='center' size='lg' noOfLines={3}>{product.name}</Heading>
-        </Tooltip>
+        <SkeletonText align='center' size='lg' noOfLines={3} isLoaded={!isLoading()}>
+          <Tooltip label={product?.name || ''}>
+            <Heading align='center' size='lg' noOfLines={3}>{product?.name}</Heading>
+          </Tooltip>
+        </SkeletonText>
         <Box mt='5'>
-          <Text noOfLines={5}>
-            {product.description}
-          </Text>
+          <SkeletonText noOfLines={5} isLoaded={!isLoading()}>
+            <Text noOfLines={5}>{product?.description}</Text>
+          </SkeletonText>
         </Box>
+        <SkeletonText mt='5' align='center' size='lg' isLoaded={!isLoading()}>
+          <Heading mt='5' align='center' size='lg'>{product?.price} €</Heading>
+        </SkeletonText>
 
-        <Heading mt='5' align='center' size='lg'>{product.price} €</Heading>
       </Box>
     </Box>
   )
