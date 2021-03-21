20bb114c2d9d18dc5f94b899a23c175e881e9460
sanchis <sanchis1992@gmail.com>
2021-03-21 22:41:52 +0100

    for the moment disable base url from axios

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import { AppLayaout } from 'components/AppLayout'
 import Spinner from 'components/Spinner'
 import '../styles/globals.css'
-import axios from 'axios'
 import theme from '../styles/theme'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
-/** SET UP BASE URL */
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? `https://${process.env.API_URL}` : `http://${process.env.API_URL}`
-/** */
 export default function MyApp ({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
