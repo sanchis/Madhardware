b2f9ecfc0faa8ad04df8fe92ee92b9e1fcfc12d4
sanchis <sanchis1992@gmail.com>
2021-06-06 16:59:47 +0200

    feat: rafactor code

--- server/db/product-shop.js
+++ server/db/product-shop.js
@@ -1,40 +1,48 @@
 import { db, onErrorDb } from './db-client'
 import { logPriceProduct } from './price-log'
 
-export async function upsertProductShop ({ pn, name, url, price, img, shop, description }) {
+export async function upsertProductShop (values) {
   return db.productShop.upsert({
     where: {
-      id: generateId(pn, shop)
+      id: generateId(values.pn, values.shop)
     },
-    update: {
-      url: url,
-      img: img,
-      price: price,
-      priceLogs: {
-        create: await logPriceProduct({ price, productShopId: generateId(pn, shop) })
-      }
-    },
-    create: {
-      id: generateId(pn, shop),
-      url: url,
-      img: img,
-      price: price,
-      product: {
-        create: {
-          id: pn,
-          name: name,
-          productDescription: {
-            create: {
-              description
-            }
+    update: await updateObjPorductShop(values),
+    create: await createObjProductShop(values)
+  }).catch(err => onErrorDb(err))
+}
+
+async function updateObjPorductShop ({ url, img, price, pn, shop }) {
+  return {
+    url: url,
+    img: img,
+    price: price,
+    priceLogs: {
+      create: await logPriceProduct({ price, productShopId: generateId(pn, shop) })
+    }
+  }
+}
+
+async function createObjProductShop ({ pn, shop, url, img, price, name, description }) {
+  return {
+    id: generateId(pn, shop),
+    url: url,
+    img: img,
+    price: price,
+    product: {
+      create: {
+        id: pn,
+        name: name,
+        productDescription: {
+          create: {
+            description
           }
         }
-      },
-      priceLogs: {
-        create: await logPriceProduct({ price, productShopId: generateId(pn, shop) })
       }
+    },
+    priceLogs: {
+      create: await logPriceProduct({ price, productShopId: generateId(pn, shop) })
     }
-  }).catch(err => onErrorDb(err))
+  }
 }
 
 function generateId (pn, shop) {
